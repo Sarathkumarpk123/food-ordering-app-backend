9953baaf2e16742993e99b2b8dc099f5dbd6a0a2
Sarathkumarpk123 <129288412+Sarathkumarpk123@users.noreply.github.com>
2024-09-05 03:06:21 +0530

    Delete models directory

--- models/cartModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cartSchema = new mongoose.Schema({
-    name: String,
-    items: Number
-    
-  });
-  const Cart = mongoose.model('Cart', cartSchema);
-  module.exports=Cart;
